1fb02bf625d3c1bb54adea47e3dde66731e29099
Tales Wallace Souza <tales.souza@aperam.com>
2017-08-08 21:40:11 -0300

    Revisão da classe Aula002b para leitura de teclado

--- Aula002b/src/com/docutm/ppoo1/aula002b/Aula002b.java
+++ Aula002b/src/com/docutm/ppoo1/aula002b/Aula002b.java
@@ -1,9 +1,17 @@
 package com.docutm.ppoo1.aula002b;
 
+import java.util.Scanner;
+
 public class Aula002b {
 
     public static void main(String[] args) {
-        System.out.println("Olá mundo!");
+        Scanner in = new Scanner(System.in);
+        System.out.print("Digite um número: ");
+        int numeroDigitado = in.nextInt();
+        System.out.println("O número digitado foi: " + numeroDigitado);
+        for(int i = 0; i < numeroDigitado; i++) {
+            System.out.print(i + ", ");
+        }
     }
     
 }
